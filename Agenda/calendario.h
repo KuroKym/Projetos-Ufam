1af5e84deaedbf0d1662140e5a87c1635453347e
KuroKym <104465246+KuroKym@users.noreply.github.com>
2023-07-31 08:11:58 -0400

    Atualizar o calendario.h

--- Agenda/calendario.h
+++ Agenda/calendario.h
@@ -1,27 +1,28 @@
-#ifndef CALENDARIO_H
-#define CALENDARIO_H
-
-typedef struct tipoEventos {
-    char nome[40];
-    char data[20];
-    char hora[10];
-} tipoEventos;
-
-typedef struct tipoNo {
-    tipoEventos evento;
-    struct tipoNo *prox;
-} tipoNo;
-
-typedef struct tipoCalendario {
-    tipoNo *prim;
-    tipoNo *ult;
-} tipoCalendario;
-
-
-void inicializaCalendario(tipoCalendario *calendario);
-void adicionaEvento(tipoCalendario *calendario, void *evento);
-void exibirEvento(tipoCalendario *calendario);
-void localizarEvento(tipoCalendario *calendario, void *chave);
-void removeEvento(tipoCalendario *calendario, void *chave);
-
-#endif
+#ifndef CALENDARIO_H 
+ #define CALENDARIO_H 
+  
+ typedef struct tipoEventos { 
+     char nome[40]; 
+     char data[20]; 
+     char hora[10]; 
+ } tipoEventos; 
+  
+ typedef struct tipoNo { 
+     tipoEventos evento; 
+     struct tipoNo *prox; 
+ } tipoNo; 
+  
+ typedef struct tipoCalendario { 
+     tipoNo *prim; 
+     tipoNo *ult;
+     int tam; 
+ } tipoCalendario; 
+  
+  
+ void criar_agenda(tipoCalendario *calendario); 
+ void adiciona_evento(tipoCalendario *calendario, void *evento); 
+ void proximo_evento(tipoCalendario *calendario); 
+ void finalizar_evento(tipoCalendario *calendario); 
+ void quantificar_evento(tipoCalendario *calendario); 
+  
+ #endif
