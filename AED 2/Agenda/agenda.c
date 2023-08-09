fed1be9728111a4918e7e80301a17d1bd56342a3
KuroKym <104465246+KuroKym@users.noreply.github.com>
2023-08-09 12:05:17 -0400

    Update agenda.c

--- AED 2/Agenda/agenda.c
+++ AED 2/Agenda/agenda.c
@@ -1,72 +1,41 @@
-#include <stdio.h> 
- #include "calendario.h" 
-  
- int main() { 
-     tipoCalendario calendario; 
-     tipoEventos evento; 
-     char continuar = 's'; 
-     int selection = 0; 
-  
-     criar_agenda(&calendario); 
-  
-  
-     while(selection >= 0 && selection <= 4){ 
-         continuar = 's'; 
-         printf("O que deseja fazer: \n"); 
-         printf("----------------------------------\n"); 
-         printf("1) Adicionar evento.\n"); 
-         printf("2) Proximo evento.\n"); 
-         printf("3) Finalizar evento\n"); 
-         printf("4) Quantificar agenda.\n");
-         printf("5) Modificar evento.\n"); 
-         printf("Digite qualquer outra coisa para sair.\n"); 
-         printf("----------------------------------\n"); 
-  
-         scanf("%d", &selection); 
-  
-         switch (selection) { 
-         case 1:
-            {
-                 printf("Digite o nome do evento: "); 
-                 scanf(" %[^\n]%*c", evento.nome); 
-  
-                 printf("Digite a data do evento (dd/mm/aaaa): "); 
-                 scanf(" %[^\n]%*c", evento.data); 
-  
-                 printf("Digite o horario do evento (xx:xx) "); 
-                 scanf(" %s", evento.hora); 
-  
-                 agendar_evento(&calendario, (void*)&evento); 
-             break; 
-                 }
-  
-         case 2: 
-             {  
-                proximo_evento(&calendario); 
-             break; 
-             } 
-  
-         case 3: 
-             finalizar_evento(&calendario); 
-             break; 
-         case 4: 
-             { 
-                 printf("%d\n", quantificar_evento(&calendario)); 
-                 break;
-             } 
-         case 5:
-         {
-            char chavedata[20];
-            char chavehora[10];
-            printf("Digite a data e hora do evento: (dd/mm/aaaa hh:mm)\n");
-            scanf(" %s%*c %[^\n]%*c", chavedata, chavehora); 
-            modificar_evento(&calendario, (void*)&chavedata, (void*)&chavehora);
-             break; 
-         }
-         default: 
-             return 0; 
-         } 
-     } 
-  
-     return 0; 
- }
+#include "stdio.h"
+#include "stdlib.h"
+#include "listase.h"
+
+typedef struct evento{
+	char tempo[20];
+	char descricao[100];
+	char prioridade[10];
+}t_evento;
+
+
+typedef struct agenda{
+	t_listase *eventos;
+	
+}t_agenda;
+
+t_agenda* criar_agenda(t_imprimir_agenda imprimir, t_comparar_agenda comparar){
+	t_agenda *agenda = malloc(sizeof(t_agenda));
+	agenda->eventos = criar_lse(NULL, NULL);
+
+	return agenda;
+}
+
+void agendar_evento(t_agenda* agenda, t_evento* evento){
+	inserir_conteudo_lse(agenda, evento);
+}
+
+
+int int main(int argc, char const *argv[])
+{
+	do{
+		scanf("%d", &cmd);
+		switch(cmd){
+		case 1:
+			agendar_evento()
+			break;
+		}
+	}
+
+	return 0;
+}
