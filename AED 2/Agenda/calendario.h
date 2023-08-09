d7407206efaa06d7c2807f9cb5347c57fe183b24
KuroKym <104465246+KuroKym@users.noreply.github.com>
2023-08-09 12:05:47 -0400

    Update calendario.h

--- AED 2/Agenda/calendario.h
+++ AED 2/Agenda/calendario.h
@@ -1,29 +1,15 @@
-#ifndef CALENDARIO_H 
-#define CALENDARIO_H 
-  
-typedef struct tipoEventos { 
-    char nome[40]; 
-    char data[20]; 
-    char hora[10]; 
-} tipoEventos; 
- 
-typedef struct tipoNo { 
-    tipoEventos evento; 
-    struct tipoNo *prox; 
-} tipoNo; 
-  
-typedef struct tipoCalendario { 
-    tipoNo *prim; 
-    tipoNo *ult;
-    int tam; 
-} tipoCalendario;
-  
-  
-void criar_agenda(tipoCalendario *calendario); 
-void agendar_evento(tipoCalendario *calendario, void *evento); 
-void proximo_evento(tipoCalendario *calendario); 
-void finalizar_evento(tipoCalendario *calendario); 
-int quantificar_evento(tipoCalendario *calendario); 
-void modificar_evento(tipoCalendario *calendario, void *chavedata, void *chavehora);
-  
-#endif
+
+typedef void (*t_imprimir_lse)(void*);
+typedef int (*t_comparar_lse)(void*, void*);
+
+typedef struct lse t_lse;
+t_lse* criar_lse(t_imprimir_lse imprimir, t_comparar_lse comparar);
+
+void inserir_lse(t_lse* lse, void* carga_util);
+void inserir_final_lse(t_lse* lse, void* carga_util);
+void inserir_conteudo_lse(t_lse* lse, void* carga_util);
+
+void* remover_lse(t_lse* lse);
+void* acessar_lse(t_lse* lse, int pos);
+
+void imprimir_lse(t_lse *lse);
