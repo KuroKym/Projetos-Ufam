e925005d0e1d9777ba5e312b076f7934debb782b
Jhonnatha Luiz de Souza Carvalho <104465246+KuroKym@users.noreply.github.com>
2025-01-08 16:03:43 -0400

    Add files via upload

---
+++ SO/Questao_1/3.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+struct carrinho {
+    int itens;
+    int valor;
+};
+
+struct carrinho car;
+
+void *adiciona(void *i) {
+    int aux;
+    printf("[ESCRITORA] Antes de adicionar| ITENS: %d VALOR: %d\n", car.itens, car.valor);
+    car.itens++;
+    aux = car.valor;
+    aux += *((int *)i);
+    sleep(1); // Simula a perda de CPU
+    car.valor += aux;
+    printf("[ESCRITORA] Depois de adicionar| ITENS: %d VALOR: %d\n", car.itens, car.valor);
+    return NULL;
+}
+
+void *retira(void *i) {
+    int aux;
+    printf("[ESCRITORA] Antes de retirar| ITENS: %d VALOR: %d\n", car.itens, car.valor);
+    car.itens--;
+    aux = car.valor;
+    aux -= *((int *)i);
+    sleep(1); 
+    car.valor -= aux;
+    printf("[ESCRITORA] Depois de retirar| ITENS: %d VALOR: %d\n", car.itens, car.valor);
+    return NULL;
+}
+
+/* void *consulta(void *arg) { 
+    printf("[LEITORA] Carrinho| ITENS: %d VALOR: %d\n", car.itens, car.valor);
+    return NULL;
+} */
+
+int main() {
+    car.itens = 0;
+    car.valor = 0;
+    int num_adiciona, num_retira; 
+    //int leitoras;
+
+   // printf("Insira a quantidade de itens que deseja adicionar, quantos deseja retirar e quantidade de threads leitoras: ");
+    printf("Insira a quantidade de itens que deseja adicionar e quantos deseja retirar: ");
+    //scanf("%d %d %d", &num_adiciona, &num_retira, &leitoras);
+    scanf("%d %d", &num_adiciona, &num_retira);
+
+    pthread_t threads[num_adiciona + num_retira /* + leitoras */];
+    int add, rm;
+
+    printf("Qual valor deseja adicionar e retirar?: ");
+    scanf("%d %d", &add, &rm);
+
+    for (int i = 0; i < num_adiciona; i++) {
+        pthread_create(&threads[i], NULL, adiciona, &add);
+    }
+
+    for (int i = num_adiciona; i < num_adiciona + num_retira; i++) {
+        pthread_create(&threads[i], NULL, retira, &rm);
+    }
+
+    /* for (int i = num_adiciona + num_retira; i < num_adiciona + num_retira + leitoras; i++) {
+        pthread_create(&threads[i], NULL, consulta, NULL); 
+    } */
+
+    for (int i = 0; i < num_adiciona + num_retira /* + leitoras */; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("VALORES FINAIS| ITENS: %d VALOR: %d\n", car.itens, car.valor);
+    return 0;
+}
